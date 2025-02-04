24846089481dd4eb8045f4cd3a9caa0fa0ebbaa5
EDeeksha <edeeksha173@gmail.com>
2025-02-04 13:47:30 +0530

    My First Commit

---
+++ src/ex_1_JavaBasics/Lab013_Println_vs_Print.java
@@ -0,0 +1,11 @@
+package ex_1_JavaBasics;
+
+public class Lab013_Println_vs_Print {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("HI");
+
+        System.out.print("Namaste");
+        System.out.print("Hyderabad");
+    }
+}
