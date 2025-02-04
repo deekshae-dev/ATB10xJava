aea8b907590f8440a33df4a1469587d53cc33622
EDeeksha <edeeksha173@gmail.com>
2025-02-04 13:41:59 +0530

    My First Commit

---
+++ src/ex_1_JavaBasics/Lab007_Comments_Shortcuts.java
@@ -0,0 +1,9 @@
+package ex_1_JavaBasics;
+
+public class Lab007_Comments_Shortcuts {
+    public static void main(String[] args){
+        //suppose if we comment,we want to remove those comments we use the shortcut
+        //cntr/
+        System.out.println("Hello all!!");
+    }
+}
---
+++ src/ex_1_JavaBasics/Lab009_Keywords_Identifiers.java
@@ -0,0 +1,10 @@
+package ex_1_JavaBasics;
+
+public class Lab009_Keywords_Identifiers {
+    public static void main(String[] args){
+        //all blue coloured are keywords
+
+        System.out.println("Hello");
+
+    }
+}
