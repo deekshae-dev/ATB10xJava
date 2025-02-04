c5c2c1ce12f5345934ce7dfd8814d0079a310979
EDeeksha <edeeksha173@gmail.com>
2025-02-04 13:39:15 +0530

    My First Commit

---
+++ src/ex_1_JavaBasics/Lab005_Comments_Multi.java
@@ -0,0 +1,10 @@
+package ex_1_JavaBasics;
+
+public class Lab005_Comments_Multi {
+    public static void main(String[] args){
+        /** author: Deeksha
+         *
+         */
+        System.out.println("Hi all");
+    }
+}
---
+++ src/ex_1_JavaBasics/Lab006_Comments1.java
@@ -0,0 +1,9 @@
+package ex_1_JavaBasics;
+
+public class Lab006_Comments1 {
+    public static void main(String[] args){
+        //this is a comment
+        //System.out.println("hi");
+    }
+
+}
