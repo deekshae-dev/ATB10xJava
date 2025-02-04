b23620eaa89692ddfda9401da3bc37a9ac17bc57
EDeeksha <edeeksha173@gmail.com>
2025-02-04 23:02:49 +0530

    My First Commit

---
+++ src/ex_2_JavaBasic/Lab014_Variables.java
@@ -0,0 +1,11 @@
+package ex_2_JavaBasic;
+
+public class Lab014_Variables {
+    public static void main(String[] args){
+        int a = 10;
+
+        int age = 65;
+        age = 65 + 1;
+        System.out.println("print the age:" +age );
+    }
+}
