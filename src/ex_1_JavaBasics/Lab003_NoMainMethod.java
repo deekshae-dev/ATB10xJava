12fd6a8adbd6d6c932a9a765b6768fe4b7ab5f9b
EDeeksha <edeeksha173@gmail.com>
2025-02-04 13:33:53 +0530

    My First Commit

---
+++ src/ex_1_JavaBasics/Lab003_NoMainMethod.java
@@ -0,0 +1,13 @@
+package ex_1_JavaBasics;
+
+public class Lab003_NoMainMethod {
+    public static void main(String[] args){
+        System.out.println("namasthe");
+    }
+
+    public static void main(){
+        System.out.println("vanakkam");
+    }
+}
+
+
