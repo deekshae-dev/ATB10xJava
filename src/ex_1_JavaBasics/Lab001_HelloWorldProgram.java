e9a06e8aee8eff668eb64912265930132e345c14
EDeeksha <edeeksha173@gmail.com>
2025-02-04 13:14:18 +0530

    My First Commit

---
+++ src/ex_1_JavaBasics/Lab001_HelloWorldProgram.java
@@ -0,0 +1,8 @@
+package ex_1_JavaBasics;
+
+public class Lab001_HelloWorldProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World , I am Deeksha!!");
+
+    }
+}
