701fe4e6d4dccb99bd0a50d2ec3c0a59477d04b2
EDeeksha <edeeksha173@gmail.com>
2025-02-09 20:53:15 +0530

    My First Commit

---
+++ src/ex_2_JavaBasic/Lab020_Constants1.java
@@ -0,0 +1,15 @@
+package ex_2_JavaBasic;
+
+public class Lab020_Constants1 {
+    public static void main(String[] args){
+
+        final int b1 = 25;
+        System.out.println(b1);
+
+        final float PI = 3.14f;
+        System.out.println(PI);
+
+        //PI = 76; value cannot be changed once assigned
+
+    }
+}
