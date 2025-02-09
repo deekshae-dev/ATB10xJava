c34a7e64ddc857d92b5b6200addea721152e6d76
EDeeksha <edeeksha173@gmail.com>
2025-02-09 17:25:28 +0530

    My First Commit

---
+++ src/ex_2_JavaBasic/Lab019_printf_ex1.java
@@ -0,0 +1,13 @@
+package ex_2_JavaBasic;
+
+public class Lab019_printf_ex1 {
+    public static void main(String[] args){
+
+        int a = 10;
+        System.out.printf("Your variable name is %d",a);
+
+        byte b = 20;
+        System.out.println();
+        System.out.printf("%d",b);
+    }
+}
