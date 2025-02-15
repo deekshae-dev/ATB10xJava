8b8185c4f01ec2d13552055cba6116b734d3bf79
EDeeksha <edeeksha173@gmail.com>
2025-02-15 15:35:21 +0530

    My First Commit

---
+++ src/ex_3_Operators/Lab038_OpEx9.java
@@ -0,0 +1,9 @@
+package ex_3_Operators;
+
+public class Lab038_OpEx9 {
+public static void main(String[] args){
+    System.out.println(!!!!(30>90));
+    //odd no of "!" values - true
+    //even no of "!" values -false
+}
+}
---
+++ src/ex_3_Operators/Lab36_OpEx8.java
@@ -0,0 +1,12 @@
+package ex_3_Operators;
+
+public class Lab36_OpEx8 {
+    public static void main(String[]args){
+
+        System.out.println(10 == 10);
+        System.out.println(10 < 10);
+        System.out.println(10 == 20);
+        System.out.println(10 <= 10);
+        System.out.println(10 >= 10);
+    }
+}
