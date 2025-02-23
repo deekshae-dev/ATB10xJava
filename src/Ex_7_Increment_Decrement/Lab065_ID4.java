d20e22c2c62e626d55a79c3cc7dc1c7b846adeac
EDeeksha <edeeksha173@gmail.com>
2025-02-23 14:08:43 +0530

    My First Commit

--- src/Ex_7_Increment_Decrement/Lab065_ID4.java
+++ src/Ex_7_Increment_Decrement/Lab065_ID4.java
@@ -1,4 +1,11 @@
 package Ex_7_Increment_Decrement;
 
 public class Lab065_ID4 {
+    public static void main(String[] args){
+        int a = 10;
+        int result = a++; //result=10 a=11
+        System.out.println(a);
+        System.out.println(result);
+
+    }
 }
