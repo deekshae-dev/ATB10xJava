04e63c202d14c1bfaa7e0b7f62b6e4a9b496d374
EDeeksha <edeeksha1703@gmail.com>
2025-04-05 11:14:07 +0530

    multi-level inheritance

---
+++ src/Ex_17_Inheritance_OOPS/Grandfather.java
@@ -0,0 +1,10 @@
+package Ex_17_Inheritance_OOPS;
+
+public class Grandfather {
+    //example of multi-level inheritance
+    void home() {
+        System.out.println("1BHK");
+    }
+    void gf() {
+        System.out.println("GF");   }
+}
