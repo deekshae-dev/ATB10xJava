a2947799e7d3ea30bd5bda34bf59fab647964833
EDeeksha <edeeksha1703@gmail.com>
2025-04-27 16:33:57 +0530

    Collections

---
+++ src/Ex_22_Collections_Framework/Ex_3_ArrayList.java
@@ -0,0 +1,11 @@
+package Ex_22_Collections_Framework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex_3_ArrayList {
+    public static void main(String[] args) {
+
+        List mylist = new ArrayList(5);
+    }
+}
